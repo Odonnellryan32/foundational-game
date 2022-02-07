7e778dc5c44f9221043004a1dd7557ada5006dc8
Ryan O'Donnell <tencee.tech@gmail.com>
2022-02-06 22:52:03 -0700

    fix(menu):fixing menu np

--- scripts/menu/Menu.gd
+++ scripts/menu/Menu.gd
@@ -5,9 +5,9 @@ extends Control
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	$btn_Singleplayer.connect("pressed", self, "Singleplayer")
-	$btn_Options.connect("pressed", self, "Options")
-	$btn_Quit.connect("pressed", self, "Quit")
+	$btn_group/btn_Singleplayer.connect("pressed", self, "Singleplayer")
+	$btn_group/btn_Options.connect("pressed", self, "Options")
+	$btn_group/btn_Quit.connect("pressed", self, "Quit")
 	$lbl_Name.connect("pressed", self, "Quit")
 	pass # Replace with function body.
 
